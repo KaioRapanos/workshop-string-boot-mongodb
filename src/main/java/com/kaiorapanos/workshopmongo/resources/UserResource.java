a5e85e111939f5e0234b7301ea7420abe75d89fd
KaioRapanos <kaiorapanos@gmail.com>
2023-06-21 20:41:31 -0300

    Endpoint to return a user's posts

--- src/main/java/com/kaiorapanos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/kaiorapanos/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kaiorapanos.workshopmongo.DTO.UserDTO;
+import com.kaiorapanos.workshopmongo.domain.Post;
 import com.kaiorapanos.workshopmongo.domain.User;
 import com.kaiorapanos.workshopmongo.services.UserService;
 
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();		
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id).orElse(null);		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
